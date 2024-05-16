b90e628d569ace2305112cc4c19c0d54e0e10506
fh <fhlqwe@gmail.com>
2024-05-16 17:39:00 +0800

    chore: Disable additional ESLint rules

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { writeFileSync } from 'node:fs'
-
+// https://github.com/biomejs/biome/blob/main/crates/biome_cli/src/execute/migrate/eslint_any_rule_to_biome.rs
 const fileUrl =
-  'https://raw.githubusercontent.com/biomejs/biome/ce4979d1ce355d74e4ace905cae6a6ff43eaba0e/crates/biome_cli/src/execute/migrate/eslint_any_rule_to_biome.rs'
+  'https://raw.githubusercontent.com/biomejs/biome/main/crates/biome_cli/src/execute/migrate/eslint_any_rule_to_biome.rs'
 
 async function generateFile(fileUrl: string) {
   const text = await fetch(fileUrl).then((res) => res.text())
